9068db500a231e52211e83aff977ef7a904cc10f
Dave Woodall <dave.woodall@livingsocial.com>
2014-10-11 20:56:48 -0600

    refactor into directives

--- script.js
+++ script.js
@@ -57,11 +57,48 @@ app.controller('bandtController', function($scope){
   };
 });
 
-app.directive('matchResults', function(){
+app.directive('winner', function(){
   return {
     restrict: 'EA',
+    controller: 'bandtController',
     replace: true,
     templateUrl: 'templates/winner.html',
   };
 });
 
+app.directive('score', function(){
+  return {
+    restrict: 'EA',
+    controller: 'bandtController',
+    replace: true,
+    templateUrl: 'templates/score_board.html'
+  };
+});
+
+app.directive('buttons', function(){
+  return {
+    restrict: 'EA',
+    controller: 'bandtController',
+    replace: true,
+    templateUrl: 'templates/buttons.html'
+  };
+});
+
+app.directive('models', function(){
+  return {
+    restrict: 'EA',
+    controller: 'bandtController',
+    replace: true,
+    templateUrl: 'templates/models.html'
+  };
+});
+
+app.directive('game', function(){
+  return {
+    restrict: 'EA',
+    controller: 'bandtController',
+    replace: true,
+    transclude: true,
+    templateUrl: 'templates/game.html'
+  };
+});
